2baf44790c4ced29e1d0c97dc5e75288117e3b9e
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-10-20 11:54:09 -0400

    Add API call and rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import patternDivider from './images/pattern-divider-desktop.svg';
 import diceIcon from './images/icon-dice.svg';
 
 function App() {
+  const [advice, setAdvice] = useState('');
+  const [slipId, setSlipId] = useState('');
+
+  const fetchRandomAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      if (data && data.slip) {
+        setAdvice(data.slip.advice);
+        setSlipId(data.slip.id);
+      } else {
+        console.error('Failed to retrieve advice from the API');
+      }
+    } catch (error) {
+      console.error('An error occurred while fetching advice:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchRandomAdvice();
+  }, []);
 
   return (
     <div className='app-container'>
       <div className='advice-container'>
-        <div className='advice-title'>ADVICE #117</div>
-        <div className='advice-body'>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</div>
+        <div className='advice-title'>ADVICE #{slipId}</div>
+        <div className='advice-body'>"{advice}"</div>
         <div className='pattern-divider'><img src={patternDivider} className='pattern-divider-image' alt='pattern divider graphic' /></div>
-        <button className='randomize-button'><img src={diceIcon} className='dice-icon' alt='dice' /></button>
+        <button className='randomize-button' onClick={fetchRandomAdvice}>
+          <img src={diceIcon} className='dice-icon' alt='dice' />
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
